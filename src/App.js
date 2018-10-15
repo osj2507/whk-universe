3967bbdafabd4c02a5150b6f24ff23c87c0853a2
osj2507 <osj@trustpilot.com>
2018-10-15 20:43:03 +0200

    adding notification about screensize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Notification from "./components/Notification.js";
 import Header from "./components/Header.js";
 import Container from "./components/Main.js";
 import Footer from "./components/Footer.js";
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <Router>
         <>
+          <Notification />
           <Header />
           <Container />
           <Footer />
---
+++ src/components/Notification.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Notification.css";
+
+function Notification() {
+  return (
+    <div className="notification-wrapper">
+      <div className="notification-content">
+        You can't see our entire universe on this screen :)<br />
+        Please switch to something bigger.
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
